3be12a0f68dfb6b76351e6ad0c750641b587ee29
acidicMercury8 <acidicmercury8@outlook.com>
2021-03-14 19:20:34 +0300

    Add empty PostprocessEditor project for Windows Forms

---
+++ src/PostprocessEditor.WindowsForms/main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PostprocessEditor.WindowsForms
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+    }
+}
