95084ca3156f6729118c0f7d596fa65a187dbdd8
acidicMercury8 <acidicmercury8@outlook.com>
2021-11-02 10:55:03 +0300

    Reconfigure application entry point

--- src/PostprocessEditor/App.xaml.cs
+++ src/PostprocessEditor/App.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using System.Windows;
 
+using PostprocessEditor.Views;
+
 namespace PostprocessEditor {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs eventArgs) {
+            base.OnStartup(eventArgs);
+
+            var window = new MainWindow();
+            window.Show();
+        }
     }
 }
