26c3c3a2fe95c50b89b946468dc38df5643abfbd
acidicMercury8 <acidicmercury8@outlook.com>
2021-11-02 13:06:59 +0300

    Add window helper module

---
+++ src/PostprocessEditor/ViewModels/WindowHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace PostprocessEditor.ViewModels {
+    public static class WindowHelper {
+        public static void CloseWindowByContext(object context) {
+            foreach (Window window in Application.Current.Windows) {
+                if (window.DataContext == context) {
+                    window.Close();
+                }
+            }
+        }
+    }
+}
