25adc8e1c5e0c4b65841ace615051cce186cbf1b
acidicMercury8 <acidicmercury8@outlook.com>
2021-11-02 13:05:37 +0300

    Add `PointList` and `TimeConstructor` user controls

---
+++ src/PostprocessEditor/Views/TimeConstructorUserControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PostprocessEditor.Views {
+    /// <summary>
+    /// Interaction logic for TimeConstructorUserControl.xaml
+    /// </summary>
+    public partial class TimeConstructorUserControl : UserControl {
+        public TimeConstructorUserControl() {
+            InitializeComponent();
+        }
+    }
+}
