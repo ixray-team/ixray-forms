f3861756409da7a9601183d8273fc408a89dcdf5
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-02 23:30:58 +0300

    Add empty color picker window

---
+++ src/PostprocessEditor.Wpf/Views/ColorPickerWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace PostprocessEditor.Wpf.Views;
+
+/// <summary>
+/// Interaction logic for ColorPickerWindow.xaml
+/// </summary>
+public partial class ColorPickerWindow : Window {
+    public ColorPickerWindow() => InitializeComponent();
+}
